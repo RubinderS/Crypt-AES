44ab5182e10911695fd773c7006726f823f06d12
Rubinder25 <rubinder.singh@nextgen.net>
2019-07-16 09:18:15 +1000

    fixed imports

--- src/commands/ProcessCommands.ts
+++ src/commands/ProcessCommands.ts
@@ -1,12 +1,12 @@
 import {CLIArgsType} from '../types';
-import {encrypt} from './Encrypt';
+import {encryptCmd} from './Encrypt';
 import {decryptCmd} from './Decrypt';
 
 function processCommands(cliArgs: CLIArgsType[]): void {
   switch (cliArgs[0].option) {
     case 'enc':
     case 'encrypt':
-      encrypt(cliArgs);
+      encryptCmd(cliArgs);
       break;
     case 'dec':
     case 'decrypt':
