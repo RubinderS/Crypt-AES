0fec317a62902648484a2938ea9273a5922c23ac
Rubinder25 <rubinder.singh@nextgen.net>
2019-07-19 11:50:49 +1000

    testing initial version

---
+++ __tests__/test.js
@@ -0,0 +1,13 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const {version, help} = require('../src/ProcessMainOptions');
+
+describe('Test 1', () => {
+  test('Test 1', async () => {
+    const {stdout, stderr} = await exec('node build/Cli.js -v');
+    if (stderr) {
+      throw 'Error: ' + stderr;
+    }
+    expect(stdout).toBe(version());
+  });
+});
