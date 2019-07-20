733b677d070a29d90181576eeb918d50569745b2
Rubinder25 <mail2rubinder@gmail.com>
2019-07-20 23:35:38 +1000

    test cases updated

--- __tests__/test.js
+++ __tests__/test.js
@@ -246,8 +246,6 @@ describe('Crypt Commands', () => {
     test('Out flag - short commands', async () => {
       const extRegex = new RegExp(`${ext}$`, 'g');
       const pathRegex = new RegExp(`${dirs.rootDir}`, 'g');
-      const encryptedFile = path.join(dirs.outPathEncDir, path.basename(dirs.fileRootDir) + ext);
-      const decryptedFile = path.join(dirs.outPathDecDir, path.basename(dirs.fileRootDir));
       const encryptCmnd = `${env} ${cliPath} enc -s ${dirs.fileRootDir} -p ${pswrd} -o ${dirs.outPathEncDir}`;
 
       await cmd(encryptCmnd);
@@ -258,40 +256,41 @@ describe('Crypt Commands', () => {
       expect(fs.existsSync(dirs.fileSubDir1)).toBe(false);
       expect(fs.existsSync(dirs.fileBlankRootDir)).toBe(false);
 
-      expect(fs.existsSync(dirs.fileRootDir.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
+      expect(fs.existsSync(dirs.fileRootDir.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(
         true,
       );
-      expect(fs.existsSync(dirs.fileDir1.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
-        true,
-      );
-      expect(fs.existsSync(dirs.fileSubDir1.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
+      expect(fs.existsSync(dirs.fileDir1.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(true);
+      expect(fs.existsSync(dirs.fileSubDir1.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(
         true,
       );
       expect(
-        fs.existsSync(dirs.fileBlankRootDir.replace(/pathRegex/g, dirs.outPathEncDir) + ext),
+        fs.existsSync(dirs.fileBlankRootDir.replace(pathRegex, dirs.outPathEncDir) + ext),
       ).toBe(true);
 
       const decryptCmnd = `${env} ${cliPath} dec -s ${dirs.outPathEncDir} -p ${pswrd} -o ${dirs.outPathDecDir}`;
       await cmd(decryptCmnd);
       expect(fs.existsSync(dirs.rootDir)).toBe(true);
 
-      expect(fs.existsSync(dirs.fileRootDir)).toBe(false);
-      expect(fs.existsSync(dirs.fileDir1)).toBe(false);
-      expect(fs.existsSync(dirs.fileSubDir1)).toBe(false);
-      expect(fs.existsSync(dirs.fileBlankRootDir)).toBe(false);
-
-      expect(fs.existsSync(dirs.fileRootDir.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
-        true,
+      expect(fs.existsSync(dirs.fileRootDir.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(
+        false,
       );
-      expect(fs.existsSync(dirs.fileDir1.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
-        true,
-      );
-      expect(fs.existsSync(dirs.fileSubDir1.replace(/pathRegex/g, dirs.outPathEncDir) + ext)).toBe(
-        true,
+      expect(fs.existsSync(dirs.fileDir1.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(false);
+      expect(fs.existsSync(dirs.fileSubDir1.replace(pathRegex, dirs.outPathEncDir) + ext)).toBe(
+        false,
       );
       expect(
-        fs.existsSync(dirs.fileBlankRootDir.replace(/pathRegex/g, dirs.outPathEncDir) + ext),
-      ).toBe(true);
+        fs.existsSync(dirs.fileBlankRootDir.replace(pathRegex, dirs.outPathEncDir) + ext),
+      ).toBe(false);
+
+      expect(fs.existsSync(dirs.fileRootDir)).toBe(true);
+      expect(fs.existsSync(dirs.fileDir1)).toBe(true);
+      expect(fs.existsSync(dirs.fileSubDir1)).toBe(true);
+      expect(fs.existsSync(dirs.fileBlankRootDir)).toBe(true);
+
+      expect(fs.readFileSync(dirs.fileRootDir, encoding)).toBe(data);
+      expect(fs.readFileSync(dirs.fileDir1, encoding)).toBe(data);
+      expect(fs.readFileSync(dirs.fileSubDir1, encoding)).toBe(data);
+      expect(fs.readFileSync(dirs.fileBlankRootDir, encoding)).toBe('');
     });
 
     test('Out and keep flag - mix commands', async () => {
