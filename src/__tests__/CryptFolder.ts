e7773a3575929cea2b079742906a879c5023bbe9
Rubinder25 <rubinder.singh@nextgen.net>
2019-07-23 09:15:38 +1000

    updated test cases

--- src/__tests__/CryptFolder.ts
+++ src/__tests__/CryptFolder.ts
@@ -34,20 +34,14 @@ describe('Crypt Commands Folder', () => {
     await cmd(`${env} ${cliPath} enc -s ${dirs.rootDir} -p ${pswrd}`);
     rootFiles.forEach((file) => {
       expect(fs.existsSync(file)).toBe(false);
-    });
-
-    rootFiles.forEach((file) => {
       expect(fs.existsSync(file + ext)).toBe(true);
     });
 
     await cmd(`${env} ${cliPath} dec -s ${dirs.rootDir} -p ${pswrd}`);
     rootFiles.forEach((file) => {
+      expect(fs.existsSync(file + ext)).toBe(false);
       expect(fs.existsSync(file)).toBe(true);
       expect(fs.readFileSync(file, encoding)).toBe(data);
-    });
-
-    rootFiles.forEach((file) => {
-      expect(fs.existsSync(file + ext)).toBe(false);
     });
   });
 
@@ -55,85 +49,77 @@ describe('Crypt Commands Folder', () => {
     await cmd(`${env} ${cliPath} encrypt --source ${dirs.rootDir} --password ${pswrd} --keep`);
     rootFiles.forEach((file) => {
       expect(fs.existsSync(file)).toBe(true);
-      deleteAll(file);
-    });
-
-    rootFiles.forEach((file) => {
       expect(fs.existsSync(file + ext)).toBe(true);
+      deleteAll(file);
     });
 
     await cmd(`${env} ${cliPath} decrypt --source ${dirs.rootDir} --password ${pswrd} --keep`);
     rootFiles.forEach((file) => {
+      expect(fs.existsSync(file + ext)).toBe(true);
       expect(fs.existsSync(file)).toBe(true);
       expect(fs.readFileSync(file, encoding)).toBe(data);
-    });
-
-    rootFiles.forEach((file) => {
-      expect(fs.existsSync(file + ext)).toBe(true);
     });
   });
 
   test('Out flag - short commands', async () => {
-    const extRegex = new RegExp(`${ext}$`, 'g');
-    const pathRegex = new RegExp(`${dirs.rootDir}`, 'g');
     const encryptCmnd = `${env} ${cliPath} enc -s ${dirs.fileRootDir} -p ${pswrd} -o ${dirs.encDir}`;
 
     await cmd(encryptCmnd);
-    expect(fs.existsSync(dirs.rootDir)).toBe(true);
+    rootFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(false);
+      expect(fs.existsSync(file + ext)).toBe(false);
+    });
 
-    expect(fs.existsSync(dirs.fileRootDir)).toBe(false);
-    expect(fs.existsSync(dirs.fileDir1)).toBe(false);
-    expect(fs.existsSync(dirs.fileSubDir1)).toBe(false);
-    expect(fs.existsSync(dirs.fileBlankRootDir)).toBe(false);
-
-    expect(fs.existsSync(dirs.fileRootDir.replace(pathRegex, dirs.encDir) + ext)).toBe(true);
-    expect(fs.existsSync(dirs.fileDir1.replace(pathRegex, dirs.encDir) + ext)).toBe(true);
-    expect(fs.existsSync(dirs.fileSubDir1.replace(pathRegex, dirs.encDir) + ext)).toBe(true);
-    expect(fs.existsSync(dirs.fileBlankRootDir.replace(pathRegex, dirs.encDir) + ext)).toBe(true);
+    encFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(false);
+      expect(fs.existsSync(file + ext)).toBe(true);
+    });
 
     const decryptCmnd = `${env} ${cliPath} dec -s ${dirs.encDir} -p ${pswrd} -o ${dirs.decDir}`;
     await cmd(decryptCmnd);
-    expect(fs.existsSync(dirs.rootDir)).toBe(true);
+    encFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(false);
+      expect(fs.existsSync(file + ext)).toBe(false);
+    });
 
-    expect(fs.existsSync(dirs.fileRootDir.replace(pathRegex, dirs.encDir) + ext)).toBe(false);
-    expect(fs.existsSync(dirs.fileDir1.replace(pathRegex, dirs.encDir) + ext)).toBe(false);
-    expect(fs.existsSync(dirs.fileSubDir1.replace(pathRegex, dirs.encDir) + ext)).toBe(false);
-    expect(fs.existsSync(dirs.fileBlankRootDir.replace(pathRegex, dirs.encDir) + ext)).toBe(false);
-
-    expect(fs.existsSync(dirs.fileRootDir)).toBe(true);
-    expect(fs.existsSync(dirs.fileDir1)).toBe(true);
-    expect(fs.existsSync(dirs.fileSubDir1)).toBe(true);
-    expect(fs.existsSync(dirs.fileBlankRootDir)).toBe(true);
-
-    expect(fs.readFileSync(dirs.fileRootDir, encoding)).toBe(data);
-    expect(fs.readFileSync(dirs.fileDir1, encoding)).toBe(data);
-    expect(fs.readFileSync(dirs.fileSubDir1, encoding)).toBe(data);
-    expect(fs.readFileSync(dirs.fileBlankRootDir, encoding)).toBe('');
+    decFiles.forEach((file) => {
+      expect(fs.existsSync(file + ext)).toBe(false);
+      expect(fs.existsSync(file)).toBe(true);
+      expect(fs.readFileSync(file, encoding)).toBe(data);
+    });
   });
 
   test('Out and keep flag - mix commands', async () => {
-    const extRegex = new RegExp(`${ext}$`, 'g');
-    const encryptedFile = path.join(dirs.encDir, path.basename(dirs.fileRootDir) + ext);
-    const decryptedFile = path.join(dirs.decDir, path.basename(dirs.fileRootDir));
     const encryptCmnd = `${env} ${cliPath} encrypt
     --source ${dirs.fileRootDir}
     -p ${pswrd}
     --output ${dirs.encDir}
     -k`.replace(/(\n|\r)/g, ' ');
     await cmd(encryptCmnd);
-    expect(fs.existsSync(dirs.fileRootDir)).toBe(true);
-    expect(fs.existsSync(dirs.fileRootDir + ext)).toBe(false);
-    expect(fs.existsSync(encryptedFile)).toBe(true);
+    rootFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(true);
+      expect(fs.existsSync(file + ext)).toBe(false);
+    });
 
+    encFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(false);
+      expect(fs.existsSync(file + ext)).toBe(true);
+    });
     const decryptCmnd = `${env} ${cliPath} decrypt
-    -s ${encryptedFile}
+    -s ${dirs.encDir}
     --password ${pswrd}
     -o ${dirs.decDir}
     --keep`.replace(/(\n|\r)/g, ' ');
     await cmd(decryptCmnd);
-    expect(fs.existsSync(encryptedFile.replace(extRegex, ''))).toBe(false);
-    expect(fs.existsSync(encryptedFile)).toBe(true);
-    expect(fs.existsSync(decryptedFile)).toBe(true);
-    expect(fs.readFileSync(decryptedFile, encoding)).toBe(data);
+    encFiles.forEach((file) => {
+      expect(fs.existsSync(file)).toBe(false);
+      expect(fs.existsSync(file + ext)).toBe(true);
+    });
+
+    decFiles.forEach((file) => {
+      expect(fs.existsSync(file + ext)).toBe(false);
+      expect(fs.existsSync(file)).toBe(true);
+      expect(fs.readFileSync(file, encoding)).toBe(data);
+    });
   });
 });
